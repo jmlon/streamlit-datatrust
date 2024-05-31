e9fd01a697aa2aa71d681a23e5c97b1174185978
Jorge M. Londoño <jmlon@yahoo.com>
2024-05-31 20:04:07 +0000

    Initiating the project

---
+++ packages.md
@@ -0,0 +1,9 @@
+Required packages:
+```sh
+pip install -U langchain langchain-community langchainhub langchain_huggingface
+pip install -U langchain-openai langchain-groq
+pip install -U pypdf docx2txt 
+pip install -U pinecone-client langchain-pinecone
+
+```
+
---
+++ rag_agent.py
@@ -0,0 +1,60 @@
+import os 
+import streamlit as st 
+
+# Models
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+
+# LangChain
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+# HuggingFace
+# from langchain.embeddings import HuggingFaceEmbeddings        # OLD
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+# Vector databases
+from langchain_pinecone import PineconeVectorStore
+
+
+index_name = "datatrust"
+# index_name = "test03-index"
+
+# LLM
+# llm = ChatOpenAI(model=os.getenv("OPENAI_MODEL"), temperature=0)
+llm = ChatGroq(model_name=st.secrets["GROQ_MODEL"], temperature=0)
+
+# Embeddings model
+EMBEDDINGS_MODEL = 'BAAI/bge-base-en-v1.5'                      # Ranking 8, 768
+embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL)
+
+# Vector store
+docsearch = PineconeVectorStore.from_existing_index(embedding=embeddings, index_name=index_name)
+retriever = docsearch.as_retriever(search_type="mmr")
+
+system_prompt = (
+    "Use the given context to answer the question."
+    "If you don't know the answer, say you don't know."
+    "Keep the answer concise and to the point."
+    "Context: {context}"
+)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+chain = create_retrieval_chain(retriever, question_answer_chain)
+
+def run_query(query):
+    return chain.invoke({"input": query})
+
+
+print("rag_agent initialized!")
+
+
+if __name__ == "__main__":
+    print("Running tests")
+    print(run_query("What is data governance?"))
